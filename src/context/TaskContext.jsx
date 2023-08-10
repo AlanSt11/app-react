f497b7abce82faba78b819d961870ed116a11e71
alan <alansaldivar1105@gmail.com>
2023-08-10 14:11:06 -0600

    first commit

---
+++ src/context/TaskContext.jsx
@@ -0,0 +1,34 @@
+import { createContext, useState, useEffect } from "react"
+import { tasks as data } from "../data/task"
+
+export const TaskContext = createContext()
+
+export function TaskContextProvider(props) {
+    const [tasks, settasks] = useState([]) // = const task = []
+
+    function createTask(task){
+        settasks([...tasks, {
+            title: task.title,
+            id: tasks.length,
+            description: task.description
+        }])
+    }
+
+    function deleteTask(taskId){
+        settasks(tasks.filter(task => task.id !== taskId))
+    }
+
+    useEffect(() => {
+        settasks(data)
+    }, [])
+
+    return (
+        <TaskContext.Provider value={{
+            tasks,
+            deleteTask,
+            createTask
+        }}>
+            {props.children}
+        </TaskContext.Provider>
+    )
+}
---
+++ src/data/task.js
@@ -0,0 +1,19 @@
+export const tasks = [
+    {
+        id: 0,
+        title: "Primer tarea",
+        description: "mi primer tarea",
+    },
+
+    {
+        id: 1,
+        title: "Primer tarea",
+        description: "mi segunda tarea",
+    },
+
+    {
+        id: 2,
+        title: "Primer tarea",
+        description: "mi tercer tarea",
+    },
+];
